ad86227e2e41804bba79a011d3265e6f38f8349a
kcaylee <carstenskaylee@gmail.com>
2024-08-29 14:09:24 +0200

    Changes made

---
+++ WebApplication/test/RegisterServletTest.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import com.example.servlets.RegisterServlet;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author carst
+ */
+public class RegisterServletTest {
+    
+    public RegisterServletTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of doPost method, of class RegisterServlet.
+     */
+    @Test
+    public void testDoPost() throws Exception {
+        System.out.println("doPost");
+        Object request = null;
+        Object response = null;
+        RegisterServlet instance = new RegisterServlet();
+        instance.doPost(request, response);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
